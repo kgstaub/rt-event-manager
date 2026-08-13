f9eca222d25c42ec96ff411ed325db0677ab3b22
Kenneth Staub <kenneth.staub@blue.works>
2026-08-13 14:27:17 +0200

    Visa: add dismiss (uk-close) button to the intro primary alert

--- includes/class-rt-event-manager-visa.php
+++ includes/class-rt-event-manager-visa.php
@@ -379,7 +379,10 @@ class RT_Event_Manager_Visa {
         $tickets = RT_Event_Manager::get_tickets_for_user($user_id);
 
         echo '<h2 class="rtacc-title uk-heading-divider">' . esc_html__('Travel and Visa', 'rt-event-manager') . '</h2>';
-        echo '<div class="uk-alert-primary" uk-alert><p>' . esc_html__('If you need a visa for Switzerland, generate a letter of invitation for each attendee below. Citizens of EU/EFTA countries do not need a visa or a letter.', 'rt-event-manager') . '</p></div>';
+        echo '<div class="uk-alert-primary" uk-alert>'
+            . '<a href class="uk-alert-close" uk-close></a>'
+            . '<p>' . esc_html__('If you need a visa for Switzerland, generate a letter of invitation for each attendee below. Citizens of EU/EFTA countries do not need a visa or a letter.', 'rt-event-manager') . '</p>'
+            . '</div>';
 
         $visa_tickets = array_filter($tickets, function ($t) {
             return in_array(RT_Event_Manager::get_ticket_kind($t), array('event', 'minor'), true);
