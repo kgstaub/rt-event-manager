4d336ac903338101de0f12ddd389eb9d6aa9f930
Kenneth Staub <kenneth.staub@blue.works>
2026-08-18 21:52:57 +0200

    Account nav: use kit-medical icon for Emergency Contact

--- includes/class-rt-event-manager-account.php
+++ includes/class-rt-event-manager-account.php
@@ -580,7 +580,7 @@ class RT_Event_Manager_Account {
         $icons = array(
             'dashboard' => 'fa-house',
             'profile'   => 'fa-user',
-            'emergency' => 'fa-suitcase-medical',
+            'emergency' => 'fa-kit-medical',
             'orders'    => 'fa-list',
             'refunds'   => 'fa-money-bill',
             'tickets'   => 'fa-ticket',
